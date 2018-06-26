b1792a5d9b93a8fc060feb6d54a3e6c20fe6a7e6
Steven C Dang <stevencdang@cmu.edu>
2018-06-26 20:46:56 +0000

    Debugged model fit code

--- mk_docker/workflow_component_local.sql
+++ mk_docker/workflow_component_local.sql
@@ -206,3 +206,29 @@ VALUES (
 	'1.0', 
 	'Allow user to evaluate the performance of a set of models'
 );
+REPLACE INTO `workflow_component` (
+	`component_type`,
+	`component_name`,
+	`tool_dir`,
+	`schema_path`,
+	`interpreter_path`,
+	`tool_path`,
+	`enabled`,
+	`author`,
+	`citation`,
+	`version`,
+	`info`
+)
+VALUES (
+	'Analysis',
+	'Model_Fit',
+	'/datashop/workflow_components/ModelFit/',
+	'/datashop/workflow_components/ModelFit/schemas/ModelFit_v1_0.xsd',
+	'/usr/bin/java -jar',
+	'/datashop/workflow_components/ModelFit/dist/ModelFit-1.0.jar', 
+	1,
+	'system',
+	'Steven_C_Dang',
+	'1.0', 
+	'Allow user to fit a model using a dataset'
+);
