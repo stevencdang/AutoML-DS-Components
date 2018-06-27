ea014b25c6a460e114e5e86bbf377fa9695f803c
Steven C Dang <stevencdang@cmu.edu>
2018-06-27 08:17:01 +0000

    succesful read and submission of produce solution request

--- modelfit/setup_run.sh
+++ modelfit/setup_run.sh
@@ -50,7 +50,7 @@ mkdir "$srcdir"/program/d3m_ta2
 find "$libdir"/d3m_ta2 -type d -printf %P\\n | xargs -I {} mkdir "$srcdir/program/d3m_ta2/{}"
 # Copy source files
 find "$libdir"/d3m_ta2 -type f -name "*.py"  -printf %P\\n | xargs -I {} cp "$libdir"/d3m_ta2/{} "$srcdir"/program/d3m_ta2/{}
-### Copy Workflow dir
+### Copy Modeling dir
 # Replicate directory structure
 mkdir "$srcdir"/program/modeling
 find "$libdir"/modeling -type d -printf %P\\n | xargs -I {} mkdir "$srcdir/program/modeling/{}"
