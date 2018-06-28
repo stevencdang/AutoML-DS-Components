b91b442934c4b8d1b17f38c5a762cf88ec089b27
Steven C Dang <stevencdang@cmu.edu>
2018-06-28 06:56:36 +0000

    adding template code for comparemodelscores

---
+++ visualizations/comparemodelscores/run_component.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Use this script to test the component locally within this folder
+
+source env/bin/activate
+
+# Setup local test folder if necessary
+cwd=$(pwd)
+if [ ! -d "$cwd/test" ]; then
+    mkdir $cwd/test
+    echo "Made test directory: '$cwd/test'"
+fi
+if [ ! -d "$cwd/test/output" ]; then
+    mkdir "$cwd/test/output"
+    echo "Made test directory: '$cwd/test/output'"
+fi
+
+# Packaging source into "program" directory
+./setup_run.sh
+
+
+# Add program directory to pythonpath
+path="$PYTHONPATH":"$cwd/program"
+
+PYTHONPATH="$path" python src/main.py -programDir $cwd -workingDir $cwd/test/output -userId=' ' -is_test=1 -file0 "$cwd/test/model-scores.tsv"
+
+deactivate
+
