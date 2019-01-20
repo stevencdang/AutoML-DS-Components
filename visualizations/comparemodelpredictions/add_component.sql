13081bd2a08a8a878bb59378f9065667872da961
Steven C Dang <stevencdang@gmail.com>
2019-01-20 06:41:17 +0000

    modified sql generation scripts for compare model generation

--- visualizations/comparemodelpredictions/add_component.sql
+++ visualizations/comparemodelpredictions/add_component.sql
@@ -1,26 +1,26 @@
-INSERT INTO `workflow_component` (
-    `component_type`, 
-    `component_name`, 
-    `tool_dir`, 
-    `schema_path`, 
-    `interpreter_path`, 
-    `tool_path`, 
-    `enabled`, 
-    `author`, 
-    `citation`, 
-    `version`, 
-    `info`
-) 
+REPLACE INTO `workflow_component` (
+	`component_type`,
+	`component_name`,
+	`tool_dir`,
+	`schema_path`,
+	`interpreter_path`,
+	`tool_path`,
+	`enabled`,
+	`author`,
+	`citation`,
+	`version`,
+	`info`
+)
 VALUES (
-    'Visualization', 
-    'Confusion_Matrix', 
-    '/datashop/workflow_components/D3mDatasetSelector/', 
-    '/datashop/workflow_components/D3mDatasetSelector/schemas/D3mDatasetSelector_v1_0.xsd', 
-    '/usr/bin/java -jar', 
-    '/datashop/workflow_components/D3mDatasetSelector/dist/D3mDatasetSelector-1.0.jar', 
-    1, 
-    'system', 
-    'Steven_C_Dang', 
-    '1.0', 
-    'Component to import a D3M Dataset into the workspace to be manipulated'
+	'Analysis',
+	'Compare_Model_Predictions',
+	'/datashop/workflow_components/CompareModelPredictions/',
+	'/datashop/workflow_components/CompareModelPredictions/schemas/CompareModelPredictions_v1_0.xsd',
+	'/usr/bin/java -jar',
+	'/datashop/workflow_components/CompareModelPredictions/dist/CompareModelPredictions-1.0.jar', 
+	1,
+	'system',
+	'Steven_C_Dang',
+	'1.0', 
+	'Create Visualization of errors of each model given'
 );
