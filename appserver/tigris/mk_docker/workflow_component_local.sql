bae61be88872deff92bb21e7fa793bdb58b11574
Steven C Dang <stevencdang@gmail.com>
2019-07-19 02:51:45 +0000

    updated sql script registering set of available components

--- appserver/tigris/mk_docker/workflow_component_local.sql
+++ appserver/tigris/mk_docker/workflow_component_local.sql
@@ -41,62 +41,6 @@ REPLACE INTO `workflow_component` (
 	`info`
 )
 VALUES (
-    2,
-	'Analysis',
-	'Dataset_Selector',
-	'/datashop/workflow_components/DatasetSelector/',
-	'/datashop/workflow_components/DatasetSelector/schemas/DatasetSelector_v1_0.xsd',
-	'/usr/bin/java -jar',
-	'/datashop/workflow_components/DatasetSelector/dist/DatasetSelector-1.0.jar', 
-	1,
-	'system',
-	'Steven_C_Dang',
-	'1.0', 
-	'Allow user to select a dataset from a dataset repository'
-);
-REPLACE INTO `workflow_component` (
-    `workflow_component_id`,
-	`component_type`,
-	`component_name`,
-	`tool_dir`,
-	`schema_path`,
-	`interpreter_path`,
-	`tool_path`,
-	`enabled`,
-	`author`,
-	`citation`,
-	`version`,
-	`info`
-)
-VALUES (
-    3,
-	'Analysis',
-	'Default_Problem_Generator',
-	'/datashop/workflow_components/ProblemGeneratorDefault/',
-	'/datashop/workflow_components/ProblemGeneratorDefault/schemas/ProblemGeneratorDefault_v1_0.xsd',
-	'/usr/bin/java -jar',
-	'/datashop/workflow_components/ProblemGeneratorDefault/dist/ProblemGeneratorDefault-1.0.jar', 
-	1,
-	'system',
-	'Steven_C_Dang',
-	'1.0', 
-	'Generate the default problem for each dataset'
-);
-REPLACE INTO `workflow_component` (
-    `workflow_component_id`,
-	`component_type`,
-	`component_name`,
-	`tool_dir`,
-	`schema_path`,
-	`interpreter_path`,
-	`tool_path`,
-	`enabled`,
-	`author`,
-	`citation`,
-	`version`,
-	`info`
-)
-VALUES (
     4,
 	'Analysis',
 	'Model_Search',
@@ -125,62 +69,6 @@ REPLACE INTO `workflow_component` (
 	`info`
 )
 VALUES (
-    5,
-	'Analysis',
-	'Problem_Task_Selector',
-	'/datashop/workflow_components/ProblemTaskSelector/',
-	'/datashop/workflow_components/ProblemTaskSelector/schemas/ProblemTaskSelector_v1_0.xsd',
-	'/usr/bin/java -jar',
-	'/datashop/workflow_components/ProblemTaskSelector/dist/ProblemTaskSelector-1.0.jar', 
-	1,
-	'system',
-	'Steven_C_Dang',
-	'1.0', 
-	'Allow user to select problem task for a given problem target'
-);
-REPLACE INTO `workflow_component` (
-    `workflow_component_id`,
-	`component_type`,
-	`component_name`,
-	`tool_dir`,
-	`schema_path`,
-	`interpreter_path`,
-	`tool_path`,
-	`enabled`,
-	`author`,
-	`citation`,
-	`version`,
-	`info`
-)
-VALUES (
-    6,
-	'Analysis',
-	'Problem_Metric_Selector',
-	'/datashop/workflow_components/ProblemMetricSelector/',
-	'/datashop/workflow_components/ProblemMetricSelector/schemas/ProblemMetricSelector_v1_0.xsd',
-	'/usr/bin/java -jar',
-	'/datashop/workflow_components/ProblemMetricSelector/dist/ProblemMetricSelector-1.0.jar', 
-	1,
-	'system',
-	'Steven_C_Dang',
-	'1.0', 
-	'Allow user to select a metric to use for the problem'
-);
-REPLACE INTO `workflow_component` (
-    `workflow_component_id`,
-	`component_type`,
-	`component_name`,
-	`tool_dir`,
-	`schema_path`,
-	`interpreter_path`,
-	`tool_path`,
-	`enabled`,
-	`author`,
-	`citation`,
-	`version`,
-	`info`
-)
-VALUES (
     7,
 	'Analysis',
 	'Model_Score',
@@ -193,62 +81,6 @@ VALUES (
 	'Steven_C_Dang',
 	'1.0', 
 	'Allow user to evaluate the performance of a set of models'
-);
-REPLACE INTO `workflow_component` (
-    `workflow_component_id`,
-	`component_type`,
-	`component_name`,
-	`tool_dir`,
-	`schema_path`,
-	`interpreter_path`,
-	`tool_path`,
-	`enabled`,
-	`author`,
-	`citation`,
-	`version`,
-	`info`
-)
-VALUES (
-    8,
-	'Analysis',
-	'Model_Fit',
-	'/datashop/workflow_components/ModelFit/',
-	'/datashop/workflow_components/ModelFit/schemas/ModelFit_v1_0.xsd',
-	'/usr/bin/java -jar',
-	'/datashop/workflow_components/ModelFit/dist/ModelFit-1.0.jar', 
-	1,
-	'system',
-	'Steven_C_Dang',
-	'1.0', 
-	'Allow user to fit a model using a dataset'
-);
-REPLACE INTO `workflow_component` (
-    `workflow_component_id`,
-	`component_type`,
-	`component_name`,
-	`tool_dir`,
-	`schema_path`,
-	`interpreter_path`,
-	`tool_path`,
-	`enabled`,
-	`author`,
-	`citation`,
-	`version`,
-	`info`
-)
-VALUES (
-    9,
-	'Analysis',
-	'Model_Predict',
-	'/datashop/workflow_components/ModelPredict/',
-	'/datashop/workflow_components/ModelPredict/schemas/ModelPredict_v1_0.xsd',
-	'/usr/bin/java -jar',
-	'/datashop/workflow_components/ModelPredict/dist/ModelPredict-1.0.jar', 
-	1,
-	'system',
-	'Steven_C_Dang',
-	'1.0', 
-	'Allow user to generate predictions on a dataset given a fitted model'
 );
 REPLACE INTO `workflow_component` (
     `workflow_component_id`,
@@ -501,32 +333,4 @@ VALUES (
 	'Steven_C_Dang',
 	'1.0', 
 	'Create exploratory data graphs for a given dataset'
-);
-REPLACE INTO `workflow_component` (
-    `workflow_component_id`,
-	`component_type`,
-	`component_name`,
-	`tool_dir`,
-	`schema_path`,
-	`interpreter_path`,
-	`tool_path`,
-	`enabled`,
-	`author`,
-	`citation`,
-	`version`,
-	`info`
-)
-VALUES (
-    19,
-	'Analysis',
-	'Dataset_Augmenter',
-	'/datashop/workflow_components/DatasetAugmenter/',
-	'/datashop/workflow_components/DatasetAugmenter/schemas/DatasetAugmenter_v1_0.xsd',
-	'/usr/bin/java -jar',
-	'/datashop/workflow_components/DatasetAugmenter/dist/DatasetAugmenter-1.0.jar', 
-	1,
-	'system',
-	'Steven_C_Dang',
-	'1.0', 
-	'Select additional datasets to augment a given dataset to improve performance on a given problem'
 );
