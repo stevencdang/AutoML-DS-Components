97d1de9995ab768671c91e5ab735bf6a187fa808
Steven C Dang <stevencdang@gmail.com>
2019-07-30 15:43:21 +0000

    - Added code for dealing with new enum types for problems, datasets, and   scores - Working test script for focusing on particular datasets crawling for   datasets grabbing default problem

--- test/src/main.py
+++ test/src/main.py
@@ -28,6 +28,29 @@ from ls_utilities.html import IframeBuilder
 
 __version__ = '0.1'
 
+test_data_names = ['DA_college_debt',
+                   'DA_medical_malpractice',
+                   'DA_ny_taxi_demand',
+                   'DA_poverty_estimation',
+                   'LL0_acled_reduced',
+                   'LL1_336_MS_Geolife_transport_mode_prediction',
+                   'LL1_336_MS_Geolife_transport_mode_prediction_separate_lat_lon',
+                   'LL1_736_population_spawn',
+                   'LL1_736_population_spawn_simpler',
+                   'LL1_penn_fudan_pedestrian',
+                   'LL1_VTXC_1343_cora',
+                   'LL1_VTXC_1369_synthetic',
+                   'SEMI_1040_sylva_prior',
+                   'SEMI_1044_eye_movements',
+                   'SEMI_1053_jm1',
+                   'SEMI_1217_click_prediction_small',
+                   'SEMI_1459_artificial_characters',
+                   'SEMI_155_pokerhand',
+                   'SUPDATA_usps_digit_classification'
+                   ]
+                   
+
+
 
 if __name__ == '__main__':
     # Parse argumennts
@@ -80,71 +103,85 @@ if __name__ == '__main__':
     ds_root = config.get_dataset_path()
     runner = DatasetImporter(db, session)
     datasets = runner.run(ds_root)
-
+    
+    logger.info("Dataset names to compare: %s" % test_data_names)
     for dsid in datasets:
         
         ds = db.get_dataset_metadata(dsid)
+        # ds_name = ds.name.lower().replace("_", " ")
+        # logger.info("Checking if %s is in list of datasets: %r" % (ds_name, ds_name in ds_names))
+        # logger.info("Cfolder folder folder folder folder folder folder hecking if folder %s is in list of datasets: %r" % (ds.dpath, any(dpath in ds.dpath for dpath in test_data_names)))
+            # logger.info("*******************************************")
+            # logger.info(ds_names[0])
+            # logger.info(ds_name)
+            # logger.info(ds_names[1])
+            # logger.info(ds_names[0] == 'da college debt')
+            # logger.info(ds_name == ds_names[0])
+            # logger.info(ds_name in ds_names)
+            # logger.info("da college debt in list: %r" % 'da college debt' in ds_names)
+            # logger.info("*******************************************")
+        if any(dpath in ds.dpath for dpath in test_data_names):
 
-        logger.info("*******************************************")
-        logger.info("Testing with dataset: %s" % ds.name)
-        logger.info("*******************************************")
+            logger.info("*******************************************")
+            logger.info("Testing with dataset: %s" % ds.name)
+            logger.info("*******************************************")
 
 
-        # Grabbing default problem
-        comp_type = "ProblemCreator"
-        comp_id = comp_type + "-1-x12345"
+            # Grabbing default problem
+            comp_type = "ProblemCreator"
+            comp_id = comp_type + "-1-x12345"
 
-        # Initialize new session
-        prob_session = ProblemCreatorSession(user_id=user_id, workflow_id=workflow_id, 
-                                       comp_type=comp_type, comp_id=comp_id)
-        prob_session.set_dataset_id(ds._id)
+            # Initialize new session
+            prob_session = ProblemCreatorSession(user_id=user_id, workflow_id=workflow_id, 
+                                           comp_type=comp_type, comp_id=comp_id)
+            prob_session.set_dataset_id(ds._id)
 
-        runner = DefaultProblemGenerator()
-        def_prob = runner.run(ds)
-        def_prob._id = db.insert_problem(def_prob)
-        prob_session.prob_id = def_prob._id
-        prob_session.add_suggestion_prob(def_prob)
-        prob_session.set_state_complete()
-        prob_session = db.add_workflow_session(prob_session)
+            runner = DefaultProblemGenerator()
+            def_prob = runner.run(ds)
+            def_prob._id = db.insert_problem(def_prob)
+            prob_session.prob_id = def_prob._id
+            prob_session.add_suggestion_prob(def_prob)
+            prob_session.set_state_complete()
+            prob_session = db.add_workflow_session(prob_session)
 
-        # Initialize model search session
-        comp_type = "ModelSearch"
-        comp_id = comp_type + "-1-x12345"
+            # Initialize model search session
+            comp_type = "ModelSearch"
+            comp_id = comp_type + "-1-x12345"
 
-        # Initialize new session
-        search_session = ModelSearchSession(user_id=user_id, workflow_id=workflow_id, 
-                                       comp_type=comp_type, comp_id=comp_id)
-        search_session.set_dataset_id(ds._id)
-        search_session.set_problem_id(def_prob._id)
-        search_session.set_input_wfids([prob_session._id])
+            # Initialize new session
+            search_session = ModelSearchSession(user_id=user_id, workflow_id=workflow_id, 
+                                           comp_type=comp_type, comp_id=comp_id)
+            search_session.set_dataset_id(ds._id)
+            search_session.set_problem_id(def_prob._id)
+            search_session.set_input_wfids([prob_session._id])
 
-        # Init the TA2 server connection
-        address = config.get_ta2_url()
-        name = config.get_ta2_name()
-        
-        logger.info("using server at address %s" % address)
-        if is_test:
-            serv = TA2Client(address, debug=True, out_dir=args.workingDir, 
-                    name=name)
-        else:
-            serv = TA2Client(address, 
-                    name=name)
-        search_session.ta2_addr = address
-        search_session = db.add_workflow_session(search_session)
+            # Init the TA2 server connection
+            address = config.get_ta2_url()
+            name = config.get_ta2_name()
+            
+            logger.info("using server at address %s" % address)
+            if is_test:
+                serv = TA2Client(address, debug=True, out_dir=args.workingDir, 
+                        name=name)
+            else:
+                serv = TA2Client(address, 
+                        name=name)
+            search_session.ta2_addr = address
+            search_session = db.add_workflow_session(search_session)
 
-        runner = ModelSearch(db, search_session, serv)
-        # m_index, models, result_df, score_data, ranked_models = runner.run(ds, prob, out_path)
-        runner.run(ds, def_prob)
-        search_session = runner.sess
+            runner = ModelSearch(db, search_session, serv)
+            # m_index, models, result_df, score_data, ranked_models = runner.run(ds, prob, out_path)
+            runner.run(ds, def_prob)
+            search_session = runner.sess
 
-        ranked_models = []
-        for rm_id in search_session.ranked_mdl_ids:
-            rm = RankedModel.from_json(db.get_object('ranked_models', rm_id))
-            logger.debug("Got ranked model from db to export: %s" % str(rm))
-            ranked_models.append(rm)
+            ranked_models = []
+            for rm_id in search_session.ranked_mdl_ids:
+                rm = RankedModel.from_json(db.get_object('ranked_models', rm_id))
+                logger.debug("Got ranked model from db to export: %s" % str(rm))
+                ranked_models.append(rm)
 
-        runner = ModelExporter()
-        runner.run(config.get_out_path(), ranked_models, serv) 
+            runner = ModelExporter()
+            runner.run(config.get_out_path(), ranked_models, serv) 
 
-        serv.end_search_solutions(search_session.search_id)
-        logger.info("Ended search solution after exporting: %s" % search_session.search_id)
+            serv.end_search_solutions(search_session.search_id)
+            logger.info("Ended search solution after exporting: %s" % search_session.search_id)
